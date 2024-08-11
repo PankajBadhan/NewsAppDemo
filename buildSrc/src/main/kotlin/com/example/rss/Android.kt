8d5f5122c58e23bb8d41fb15322a10513e958a11
Pankaj <pkbadhan27@gmail.com>
2024-08-11 13:47:53 -0600

    Initial Commit NewsApp

---
+++ buildSrc/src/main/kotlin/com/example/rss/Android.kt
@@ -0,0 +1,10 @@
+package com.example.rss
+
+object Android {
+    const val minSdkVersion = 21
+    const val targetSdkVersion = 30
+    const val compileSdkVersion = 30
+    const val applicationId = "com.example.rss"
+    const val versionCode = 1
+    const val versionName = "1"
+}
---
+++ data/src/main/java/com/example/rss/data/repository/jsonFeed/JsonFeedRepositoryImpl.kt
@@ -0,0 +1,45 @@
+package com.example.rss.data.repository.jsonFeed
+
+import com.example.rss.data.di.Cloud
+import com.example.rss.data.source.cloud.BaseCloudRepository
+import com.example.rss.data.source.db.dao.JsonFeedDao
+import com.example.rss.domain.model.jsonFeed.ArticleModel
+import com.example.rss.domain.repository.jsonFeed.JsonFeedRepository
+import io.reactivex.Flowable
+import javax.inject.Inject
+
+class JsonFeedRepositoryImpl @Inject constructor(
+    @Cloud private val cloudRepository: BaseCloudRepository,
+    private val jasonFeedDao: JsonFeedDao
+) : JsonFeedRepository {
+
+    override fun getJsonFeed(sources: String, apiKey: String): Flowable<List<ArticleModel>> {
+        return Flowable.fromCallable { jasonFeedDao.getCount() }
+            .flatMap {
+                if (it == 0)
+                // nothing exists on database, so get them from cloud
+                    getJsonFeedFromCloud(sources, apiKey).flatMap {
+                        jasonFeedDao.getJsonFeed()
+                    }
+                else
+                // there are some movies in db, so attempt to get them and update db
+                    Flowable.concatArrayEager(jasonFeedDao.getJsonFeed(), getJsonFeedFromCloud(sources, apiKey))
+            }
+    }
+
+    private fun getJsonFeedFromCloud(sources: String, apiKey: String): Flowable<List<ArticleModel>> {
+        return cloudRepository
+            .getJsonFeed(sources,apiKey).map {
+                it.articles
+            }.map(this::insertToJsonFeedDb)
+    }
+
+    /**
+     * Attempts to insert fetched jsonFeed to database and returns the given list
+     */
+    private fun insertToJsonFeedDb(jsonFeed: List<ArticleModel>): List<ArticleModel> {
+        jasonFeedDao.insertJsonFeed(jsonFeed)
+        return jsonFeed
+    }
+
+}
